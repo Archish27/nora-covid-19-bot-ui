d27f5c1fe813968b3cc717ca6417533cfe23d561
Archish Thakkar <archishthakkar@gmail.com>
2020-08-29 14:09:05 +0530

    Added for fork me badge & vulnerability fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { CustomWidget } from "../components/common/CustomWidget";
 import Homepage from "./Home";
+import ForkMeOnGithub from "fork-me-on-github";
 
 const Index = () => (
   <>
     <>
       <CustomWidget />
       <Homepage key="1" />
+      <ForkMeOnGithub
+        repo="https://github.com/Archish27/nora-covid-19-bot"
+        colorBackground="#135afe"
+        colorOctocat="white"
+      />
     </>
   </>
 );
